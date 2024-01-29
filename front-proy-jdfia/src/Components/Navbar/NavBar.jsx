4034eb8e29740947bb82c971ed3427032baa7e13
Alexzp20 <71680855+Alexzp20@users.noreply.github.com>
2024-01-29 15:36:22 -0600

    Fix: estructura del navBar
    Modificación de la composición del NavBar, añadiendo nuevos campos y modificando el espaciado entre ellos

--- front-proy-jdfia/src/Components/Navbar/NavBar.jsx
+++ front-proy-jdfia/src/Components/Navbar/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import { Container, Navbar, NavbarBrand, NavbarText, Row,Col } from 'reactstrap';
+import { Container, Navbar, NavbarBrand, NavbarText, Row,Col, Nav, NavItem } from 'reactstrap';
 
 const NavBar = () => {
     return (
@@ -12,30 +12,36 @@ const NavBar = () => {
             light
             >
             <Container fluid>
-                <Row className='border' >
-                    <Col className='border' xs="6">
+                <Row >
+                    <Col xs="6">
                         <NavbarBrand>
                         <Link style={{ textDecoration: 'none', color: 'white'}} to="/inicio" >Junta Directiva</Link>
                         </NavbarBrand>
 
                     </Col>
-                    <Col className='border text-center' xs="1">
-                        <NavbarText><Link style={{ textDecoration: 'none' }} to="/inicio" >Inicio</Link></NavbarText>                              
-                    </Col>
-                    <Col className='border text-center' xs="1">
-                        <NavbarText><Link style={{ textDecoration: 'none' }} to="/" >Perfil</Link></NavbarText>                              
-                    </Col>
-                    <Col className='border text-left' xs="1">
-                        <NavbarText><Link style={{ textDecoration: 'none' }} to="/" >notificaciones</Link></NavbarText>                              
-                    </Col>
-                    <Col className='border text-center' xs="1">
-                        <NavbarText><Link style={{ textDecoration: 'none' }} to="/" ></Link></NavbarText>                              
-                    </Col>
-                    <Col className='border text-center' xs="1">
-                        <NavbarText><Link style={{ textDecoration: 'none' }} to="/" ></Link></NavbarText>                              
-                    </Col>
-                    <Col className='border text-center' xs="1"  >
-                        <NavbarText><Link className= 'text-nowrap' style={{ textDecoration: 'none'}} to="/" >Cerrar Sesion</Link></NavbarText>                              
+                    <Col xs="6">
+                        <Nav fill>
+                        <NavItem>
+                            <NavbarText><Link style={{ textDecoration: 'none' }} to="/inicio" >Inicio</Link></NavbarText>                              
+                        </NavItem>
+
+                        <NavItem>
+                            <NavbarText><Link style={{ textDecoration: 'none' }} to="/" >Perfil</Link></NavbarText>                              
+                        </NavItem>
+
+                        <NavItem>
+                            <NavbarText><Link style={{ textDecoration: 'none' }} to="/" >notificaciones</Link></NavbarText>                              
+                        </NavItem>
+                        <NavItem>
+                            <NavbarText><Link style={{ textDecoration: 'none' }} to="/" >Integrantes</Link></NavbarText>                              
+                        </NavItem>
+                        <NavItem>
+                            <NavbarText><Link style={{ textDecoration: 'none' }} to="/" >Nosotros</Link></NavbarText>                              
+                        </NavItem>
+                        <NavItem>
+                            <NavbarText><Link className= 'text-nowrap' style={{ textDecoration: 'none'}} to="/" >Cerrar Sesion</Link></NavbarText>                              
+                        </NavItem>
+                        </Nav>
                     </Col>
                 </Row>
             </Container>
