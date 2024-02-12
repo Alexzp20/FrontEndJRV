7f698b786ebe42d60280ae4cf40929a55329432f
Alexzp20 <71680855+Alexzp20@users.noreply.github.com>
2024-02-12 16:36:18 -0600

    Fix: Estructura de la pantalla de inicio de sesión
    Modificación de la estructura de los inputs, colores, se añadió la imagen del escudo de la universidad

--- front-proy-jdfia/src/Components/InicioSesion/InicioSesion.jsx
+++ front-proy-jdfia/src/Components/InicioSesion/InicioSesion.jsx
@@ -1,56 +1,90 @@
 import React from 'react';
-import { Button, Container,  Input, Label, Row, Col} from 'reactstrap';
+import { Button, Container, Input, Label, Row, Col,Form} from 'reactstrap';
+import {useForm, Controller} from 'react-hook-form'; 
+import { useNavigate } from 'react-router-dom';
 
 const InicioSesion = () => {
+
+    const {handleSubmit, control} = useForm();
+    const navigate = useNavigate();
+
+    const onSubmit = (data) =>{
+        console.log(data)
+         navigate("/inicio");
+        }
     return (
-        <div>
+        <Container fluid>
             <br />
             <br />
             <br />
-            <Container className=' bg-black rounded bg-opacity-75'>
-                <br />
-                <Row>
-                <h2 className='text-center text-light'>Iniciar Sesion</h2>
-                </Row>
-                    <Container>
+            <Row>
+                <Col xs="2"></Col>
+                <Col xs="8">
+                    <Container className=' bg-secondary rounded bg-opacity-75'> 
+                        <br />
                         <Row>
-                            <Col xs="3" ></Col>
-                            <Col xs="6" >
-                            <Label className='text-light' for="user"> Nombre de usuario </Label>
-                                <Input 
-                                bsSize="sm"
-                                className="mb-3" 
-                                id="user"
-                                name="userText"
-                                placeholder="Ingrese su usuario"
-                                type="text"
-                                />
-                                <Label className='text-light' for="user"> Contraseña </Label>
-                                <Input  
-                                bsSize="sm"
-                                className="mb-3"
-                                id="password"
-                                name="userText"
-                                placeholder="Ingrese su Contraseña"
-                                type="password"
-                                />
+                            <h2 className='text-center text-light'>Iniciar Sesion</h2>
+                        </Row>
+                        <br /> 
+                        <Row>
+                            <Col xs="1"></Col>
+                            <Col xs="3" >
+                            <img src="/images/inicioSesion/sesion.png" className= "img-fluid" alt="" />
                             </Col>
-                            <Col xs="3" ></Col>
+                            <Col xs="1"></Col>
+                            <Col xs="6" >
+                                <br />
+                                <Form onSubmit={handleSubmit(onSubmit)}>
+                                    <Label className='text-light' for="user"> Nombre de usuario </Label>
+                                    <Controller
+                                                name="userText"
+                                                control={control}
+                                                defaultValue=""
+                                                render={({ field }) => 
+                                                <Input
+                                                    {...field} 
+                                                    bsSize="sm"
+                                                    className="mb-3 " 
+                                                    id="user"
+                                                    placeholder="Ingrese su usuario"
+                                                    type="text"
+                                                />}
+                                        />  
+
+                                    <Label className='text-light' for="password"> Contraseña </Label>
+                                    <Controller
+                                                name="userPass"
+                                                control={control}
+                                                defaultValue=""
+                                                render={({ field }) => 
+                                                <Input  
+                                                    {...field}
+                                                    bsSize="sm"
+                                                    className="mb-3"
+                                                    id="password"
+                                                    placeholder="Ingrese su Contraseña"
+                                                    type="password"
+                                                />}
+                                        />  
+                                    <br />
+                                    <Row>
+                                        <Col xs='4'></Col>
+                                        <Col xs='5'>
+                                        <Button color="success" type='submit'>
+                                                Iniciar Sesion
+                                        </Button>
+                                        </Col>
+                                    </Row>
+                                </Form>
+                            </Col>
                         </Row>
                         <br />
-                                <Row>
-                                    <Col xs='5'></Col>
-                                    <Col xs='2'>
-                                        <Button color="success">
-                                            Iniciar Sesion
-                                        </Button>
-                                    </Col>
-                                    <Col xs='5'></Col>
-                                </Row>
-                                <br />
-                    </Container>
+                        <br />
             </Container>
-        </div>
+                </Col>
+                <Col xs="2"></Col>
+            </Row>
+        </Container>
     );
 }
 
